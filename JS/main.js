cad1dd002cced7de0b6e90ccb3f544c8b3bae436
alopezbelles <alopezbelles@gmail.com>
2022-10-12 11:48:26 +0200

    cambios de nombres en las id y las classes, del 0 al 8, para identidicar las posiciones en matriz

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,30 @@
 ///ARRAYS///
 //Arrays ganadoras//
 
-// let arrayWinner = [[1,2,3],[4,5,6],,[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+
+const winConditions = {
+    pos1: ["caja0","caja1","caja2"],
+    pos2: ["caja3","caja4","caja5"],
+    pos3: ["caja6","caja7","caja8"],
+    pos4: ["caja0","caja3","caja6"],
+    pos5: ["caja1","caja4","caja7"],
+    pos6: ["caja2","caja5","caja8"],
+    pos7: ["caja0","caja4","caja8"],
+    pos8: ["caja2","caja4","caja6"],
+
+};
+
+
+
+
+
+// let player1Mark = `X`;
+// let player2Mark = `O`;
+
+
+const cells = document.querySelectorAll(".cuadrado");
+const statusText = document.querySelector("#statusText")
+
 
 //////////////////////////////////
 
@@ -23,65 +46,65 @@ let interruptor = true;
 const marcarCasilla = (posicion) => {
 
     switch(posicion){
+        case '0':
+            if(caja0.innerHTML == ""){
+                caja0.innerHTML = (interruptor) ?  `<h1>X</h1>` : `<h1>O</h1>`;
+                interruptor = !interruptor;
+            };
+        break;
+
         case '1':
             if(caja1.innerHTML == ""){
-                caja1.innerHTML = (interruptor) ?  `X` : `O`;
+                caja1.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '2':
             if(caja2.innerHTML == ""){
-                caja2.innerHTML = (interruptor) ? `X` : `O`;
+                caja2.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '3':
             if(caja3.innerHTML == ""){
-                caja3.innerHTML = (interruptor) ? `X` : `O`;
+                caja3.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '4':
             if(caja4.innerHTML == ""){
-                caja4.innerHTML = (interruptor) ? `X` : `O`;
+                caja4.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '5':
             if(caja5.innerHTML == ""){
-                caja5.innerHTML = (interruptor) ? `X` : `O`;
+                caja5.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '6':
             if(caja6.innerHTML == ""){
-                caja6.innerHTML = (interruptor) ? `X` : `O`;
+                caja6.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '7':
             if(caja7.innerHTML == ""){
-                caja7.innerHTML = (interruptor) ? `X` : `O`;
+                caja7.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
 
         case '8':
             if(caja8.innerHTML == ""){
-                caja8.innerHTML = (interruptor) ? `X` : `O`;
-                interruptor = !interruptor;
-            };
-        break;
-
-        case '9':
-            if(caja9.innerHTML == ""){
-                caja9.innerHTML = (interruptor) ? `X` : `O`;
+                caja8.innerHTML = (interruptor) ? `<h1>X</h1>` : `<h1>O</h1>`;
                 interruptor = !interruptor;
             };
         break;
@@ -90,6 +113,15 @@ const marcarCasilla = (posicion) => {
             console.log("error");
     };
 };
+
+
+///////////////////////////
+
+if (("caja1"==="X") && ("caja2"==="X") && ("caja3"==="X")) {
+    console.log("winner");
+}
+
+
 
 // let caja1 = document.getElementById("1");
 // let caja2 = document.getElementById("2");
